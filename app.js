bb33173be36bfbb3ead4cc483b3df00680bc98d4
johnqt1k <john.sehlstrom@edu.huddinge.se>
2023-10-03 15:45:49 +0200

    Added exercise

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var indexRouter = require('./routes/index');
+var aboutRouter = require('./routes/about');
+var usersRouter = require('./routes/users');
 
 var app = express();
 
@@ -15,6 +18,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/about', aboutRouter);
+app.use('/users', usersRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- app.test.js
+++ app.test.js
@@ -1,23 +1,48 @@
 const request = require('supertest');
 const app = require('./app');
 
-test('I am alive and responding', done => {
+describe('sites running', ( )=> { 
+    test('I am alive and responding', done => {
     request(app)
     .get('/')
     .expect(200)
     .end(done)
 });
 
-test('GET /', done => {
+    test('GET /', done => {
     request(app)
     .get('/about')
     .expect(200)
     .end(done)
 });
 
-test('GET jibberish', done => {
+    test('GET jibberish', done => {
     request(app)
     .get('/asjdgajdsg')
     .expect(404)
     .end(done)
+    });
+});
+
+describe('user tests', () => {
+    test('GET /users', done => {
+        request(app)
+            .get('/users')
+            .expect(200)
+            .end(done);
+    });
+
+    test('GET users/10', done => {
+        request(app)
+            .get('/users/10')
+            .expect(404) 
+            .end(done);
+    });
+
+    test('DELETE /users/10 removes the user with the id 10', done => {
+        request(app)
+            .delete('/users/10') // Assuming you want to test a delete operation
+            .expect(404) // Adjust the expected status code based on your application's behavior
+            .end(done);
+    });
 });
